77a396989a6309b3e93a77fbd3f8d0b8a381d1b6
linghan <ryou0634@gmail.com>
2018-08-31 11:39:21 +0900

    add None option for gamma in GradReverseMLP.

--- torch_models/models/grad_reverse_mlp.py
+++ torch_models/models/grad_reverse_mlp.py
@@ -12,9 +12,10 @@ class GradReverseMLP(MLP):
     Attributes
     ----------
 
-    gamma : int (float) > 0
+    gamma : int (float) > 0 or None
         A hyper-parameter to control the growth of lambda.
         The bigger it is, the faster lambda grows.
+        If None, get_lamd() always returns 1.
     max_update : int
         A hyper-parameter to control the growth of lambda.
         The bigger it is, the slower lambda grows.
@@ -23,7 +24,7 @@ class GradReverseMLP(MLP):
         The number of updates. It is incremented every time self.get_lambd() is called.
         The bigger it is, the closer to 1 lambda is.
     '''
-    def __init__(self, dims, activation='Tanh', dropout=0, gamma=10, max_update=50):
+    def __init__(self, dims, activation='Tanh', dropout=0, gamma=None, max_update=50):
         super().__init__(dims, activation=activation, dropout=dropout)
         self.gamma = gamma
         self.max_update = max_update
@@ -35,4 +36,7 @@ class GradReverseMLP(MLP):
 
     def get_lambd(self):
         p = self.n_update/self.max_update
-        return 2/(1+np.exp(-self.gamma*p)) - 1
+        if self.gamma:
+            return 2/(1+np.exp(-self.gamma*p)) - 1
+        else:
+            return 1
