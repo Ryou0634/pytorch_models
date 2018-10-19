7f49cea566b26e9550b7d8b22346edefde4ee948
linghan <ryou0634@gmail.com>
2018-10-19 18:43:55 +0900

    train_scheduling

--- torch_models/scripts/train_scheduling.py
+++ torch_models/scripts/train_scheduling.py
@@ -4,18 +4,19 @@ from torch_models.utils import PyTorchModelSaver
 from my_utils.misc.logging import logger
 
 
-go_up_dict = {'loss': False, 'accuracy': True, 'BLEU': True}
-eval_dict = {'loss': EvaluatorLoss, 'accuracy': EvaluatorC, 'BLEU': EvaluatorSeq}
+go_up_dict = {'loss': False, 'accuracy': True, 'BLEU': True, 'sent_BLEU': True}
+eval_dict = {'loss': EvaluatorLoss, 'accuracy': EvaluatorC,
+             'BLEU': EvaluatorSeq, 'sent_BLEU': EvaluatorSeq}
 
 def train_lr_decay(model, train_loader, dev_loader, measure,
                    checkpoint_steps=5000, max_steps=100000,
                    optimizer='Adam', initial_lr=0.1, decay_rate=0.5, lr_threshold=1e-6,
-                   save_path=None):
+                   save_path=None, tenacity=1):
 
     # Preparing
     trainer = Trainer(model, train_loader)
-    dev_evaluator = eval_dict[measure](model, dev_loader)
-    score_monitor = ScoreMonitor(threshold=1, go_up=go_up_dict[measure])
+    dev_evaluator = eval_dict[measure](model, dev_loader, measure=measure)
+    score_monitor = ScoreMonitor(tenacity=tenacity, go_up=go_up_dict[measure])
     model_saver = PyTorchModelSaver(model, save_file=save_path) if save_path else None
 
     optimizer = eval(optimizer)(model.parameters(), lr=initial_lr)
